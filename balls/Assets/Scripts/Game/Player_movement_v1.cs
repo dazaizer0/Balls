dab9920bdddfbde8ad9b53754b8a4cecc2334573
dazai <mydataisnotforfbi@proton.me>
2023-03-25 15:22:36 +0100

    prototyp

--- balls/Assets/Scripts/Game/Player_movement_v1.cs
+++ balls/Assets/Scripts/Game/Player_movement_v1.cs
@@ -1,12 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_movement_v1 : MonoBehaviour
 {
-    Rigidbody rb;
+
+    public float speed = 5f;
+    public float jump_force = 10f;
+    public bool isGrounded;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        Vector2 axis = new Vector2(Input.GetAxis("Vertical"), Input.GetAxis("Horizontal")) * speed;
+        Vector3 forward = new Vector3(-Camera.main.transform.right.z, 0, Camera.main.transform.right.x);
+        Vector3 wishDirection = (forward * axis.x + Camera.main.transform.right * axis.y + Vector3.up * rb.velocity.y);
+        
+        rb.velocity = wishDirection;
+    }
+    
+    void OnCollisionStay()
+    {
+    
+        isGrounded = true;
+    }
+
+
+    /*Rigidbody rb;
     public float jump_delta_v;
-    public float accel;
+    public float accel;s
 
     void Start()
     {
@@ -19,5 +47,5 @@ public class Player_movement_v1 : MonoBehaviour
         rb.AddForce(((Input.GetKey(KeyCode.W) ? 1 : 0) - (Input.GetKey(KeyCode.S) ? 1 : 0)) * accel * transform.forward, ForceMode.Acceleration);
         rb.AddForce(((Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0)) * accel * transform.right , ForceMode.Acceleration);
         rb.AddForce(((Input.GetKey(KeyCode.Space) ? 1 : 0) - (Input.GetKey(KeyCode.LeftShift) ? 1 : 0)) * jump_delta_v * transform.up , ForceMode.Acceleration);
-    }
+    }*/
 }
